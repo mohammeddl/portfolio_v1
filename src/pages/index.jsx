0be04ba3b1723cff3a8da9ec1cb159f180ba1b8e
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-04-05 01:06:59 +0000

    end

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -43,24 +43,6 @@ function MailIcon(props) {
 export default function About() {
   return (
     <>
-      <NextSeo
-        title="About - Daali Mohammed"
-        description={siteMeta.description}
-        canonical="https://brian.dev/about"
-        openGraph={{
-          url: 'https://brian.dev/about',
-          images: [
-            {
-              url: `https://og.brian.dev/api/og?title=About&desc=${siteMeta.description}`,
-              width: 1200,
-              height: 600,
-              alt: 'Og Image Alt',
-              type: 'image/jpeg',
-            }
-          ],
-          siteName: 'brian.dev',
-        }}
-      />
       <Container className="mt-16 sm:mt-32">
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
